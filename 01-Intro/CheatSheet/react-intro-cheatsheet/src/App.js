2e6ba65cc5bc7c303f0a30fe6bb6202eb0319e31
Randkill <randkill.randkill@gmail.com>
2020-08-10 22:31:03 +0430

    Used React Hooks instead of static states!

--- 01-Intro/CheatSheet/react-intro-cheatsheet/src/App.js
+++ 01-Intro/CheatSheet/react-intro-cheatsheet/src/App.js
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/person.js'
-class App extends Component { //class can t be used, for instead, className is uesed!
-  state = {
+
+const App = (props) => { //class can t be used, for instead, className is uesed!
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: "Erfan", age: "22"},
       {name: "ًRandkill", age: "23"}
     ]
-  }
+  })
 
-  swithcNameHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState({otherState: "Some other state"})  //it could be an object or even text, 
+                                                                                    //also we can have as many useState()s as we want
+
+  const swithcNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: "Erfan Randkill", age: "23"},
         {name: "ًRandkill", age: "23"}
       ]
     })
   }
-  render() {
+
+  console.log(personsState, otherState)
+
+  
     return (
       <div className="App">
         <h1>Hello, this is my first React app!</h1>
         <p>Nicely working! </p>
-        <button onClick={this.swithcNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hei there</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[0].age}></Person>
+        <button onClick={swithcNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Hei there</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I m called React')); alternative way
-  }
-uu}
+}
 
 export default App;
