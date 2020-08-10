35236c4594b04ebe30ca8b14d0104b8d06e3b5e8
Randkill <randkill.randkill@gmail.com>
2020-08-10 16:47:51 +0430

    Person component created and an instance had been added to App.js

--- 01-Intro/CheatSheet/react-intro-cheatsheet/src/App.js
+++ 01-Intro/CheatSheet/react-intro-cheatsheet/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
-class App extends Component {
+import Person from './Person/person.js'
+class App extends Component { //class can t be used, for instead, className is uesed!
   render() {
     return (
       <div className="App">
         <h1>Hello, this is my first React app!</h1>
+        <p>Nicely working!</p>
+        <Person/>
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I m called React')); alternative way
