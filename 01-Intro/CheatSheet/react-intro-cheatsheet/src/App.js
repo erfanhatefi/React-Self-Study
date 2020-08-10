9a28cd5b14b96d51322f8573fa6f1c9e37d646c7
Randkill <randkill.randkill@gmail.com>
2020-08-10 16:29:32 +0430

    JSX replaced with new smaller one and its alternative, React.createElement(.....)

--- 01-Intro/CheatSheet/react-intro-cheatsheet/src/App.js
+++ 01-Intro/CheatSheet/react-intro-cheatsheet/src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello, this is my first React app!</h1>
       </div>
     );
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I m called React')); alternative way
   }
-}
+uu}
 
 export default App;
