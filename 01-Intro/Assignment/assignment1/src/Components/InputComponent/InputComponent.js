841356e772558e1a388004c8323be9b07ccd7f74
Randkill <randkill.randkill@gmail.com>
2020-08-11 18:44:05 +0430

    Input Component initialized!

---
+++ 01-Intro/Assignment/assignment1/src/Components/InputComponent/InputComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputComponent = (props) => {
+    return(
+        <div>
+            <input type='text'></input>
+        </div>
+    )
+}
+
+export default InputComponent;
