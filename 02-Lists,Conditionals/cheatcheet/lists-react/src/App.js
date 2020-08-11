fca223e9990dd459bd439fa0aa1bbd0d32cebfb4
Randkill <randkill.randkill@gmail.com>
2020-08-11 19:58:06 +0430

    Took everything from ../01-Intro/Cheatsheet, as to continue our path and get deep into it!

---
+++ 02-Lists,Conditionals/cheatcheet/lists-react/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/person.js'
+class App extends Component { //class can t be used, for instead, className is uesed!
+  state = {
+    persons: [
+      {name: "Erfan", age: "22"},
+      {name: "ًRandkill", age: "23"}
+    ]
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: "23"},
+        {name: "ًRandkill", age: "23"}
+      ]
+    })
+  }
+
+  swithcNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: "23"},
+        {name: "ًRandkill", age: "23"}
+      ]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, this is my first React app!</h1>
+        <p>Nicely working! </p>
+        <button onClick={() => this.swithcNameHandler('Randkillli')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.swithcNameHandler}
+          change={this.nameChangeHandler}
+        >Hei there</Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[0].age}
+          click={this.swithcNameHandler.bind(this, 'Randkilllllll')}
+        ></Person>
+      </div>
+    );
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I m called React')); alternative way
+  }
+}
+
+export default App;
