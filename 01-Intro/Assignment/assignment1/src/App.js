676dbc981c42e1650fe67dc2d110fdd528a0a302
Randkill <randkill.randkill@gmail.com>
2020-08-11 19:02:12 +0430

    Two Components + state + userNameChangeHandler added!

--- 01-Intro/Assignment/assignment1/src/App.js
+++ 01-Intro/Assignment/assignment1/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputComponent from './Components/InputComponent/InputComponent.js'
+import OutputComponent from './Components/OutputComponent/OutputComponent.js'
 
 class App extends Component {
+
+  state = {
+    name: 'Randkill'
+  }
+
+  userNameChangeHandler = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +24,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <h1>Assignment 1</h1>
+        <InputComponent change={this.userNameChangeHandler} username={this.state.name}/>
+        <OutputComponent username={this.state.name}/>
+        <OutputComponent username={this.state.name}/>
+
       </div>
     );
   }
