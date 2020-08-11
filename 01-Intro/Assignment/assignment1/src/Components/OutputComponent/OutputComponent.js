b781e893f321756171eab7695e7d39cd34b19503
Randkill <randkill.randkill@gmail.com>
2020-08-11 18:45:09 +0430

    InputComponent.js deleted, renamed cause acutally it was OutputCoponent.jsgit add assignment1/src/Components/OutputComponent/IutputComponent.js

---
+++ 01-Intro/Assignment/assignment1/src/Components/OutputComponent/OutputComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OutputComponent = (props) => {
+    return(
+        <div>
+            <h1>output 1</h1>
+        </div>
+    )
+}
+
+export default OutputComponent;
