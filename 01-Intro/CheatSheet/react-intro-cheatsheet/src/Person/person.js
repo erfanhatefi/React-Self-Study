6f05a4fffde87d112d39e11f3d2242cbe605ecca
Randkill <randkill.randkill@gmail.com>
2020-08-11 17:05:23 +0430

    Input box with VALUE and ONCHANGE handler added!

--- 01-Intro/CheatSheet/react-intro-cheatsheet/src/Person/person.js
+++ 01-Intro/CheatSheet/react-intro-cheatsheet/src/Person/person.js
@@ -5,6 +5,7 @@ const person = (props) => {
         <div>
             <p onClick={props.click}>I`m  {props.name} and I`m {props.age} years old</p>
             <p>{props.children}</p>
+            <input onChange={props.change} value={props.name}/>
         </div>
     )
 }
