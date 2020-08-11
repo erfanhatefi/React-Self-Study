d614c7f094a355b9d94431a718286d18edb30200
Randkill <randkill.randkill@gmail.com>
2020-08-11 19:01:18 +0430

    onChange status added to component, works dynamicly!

--- 01-Intro/Assignment/assignment1/src/Components/InputComponent/InputComponent.js
+++ 01-Intro/Assignment/assignment1/src/Components/InputComponent/InputComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const InputComponent = (props) => {
     return(
         <div>
-            <input type='text'></input>
+            <input type='text' onChange={props.change} value={props.username}></input>
         </div>
     )
 }
