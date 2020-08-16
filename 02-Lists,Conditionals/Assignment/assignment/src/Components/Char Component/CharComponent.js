4aa77772d02e3c91f6d4b89a98ee5d4c8c9f7f9f
Randkill <randkill.randkill@gmail.com>
2020-08-16 12:51:11 +0430

    CharComponent.js edited and completed, outputs each character!

--- 02-Lists,Conditionals/Assignment/assignment/src/Components/Char Component/CharComponent.js
+++ 02-Lists,Conditionals/Assignment/assignment/src/Components/Char Component/CharComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './CharComponent.css'
 
 const char = (props) => {
-    
-    reutnr (
-        <div>
 
+    return (
+        <div className='Char'>
+            {props.character}
         </div>
     )
 }
