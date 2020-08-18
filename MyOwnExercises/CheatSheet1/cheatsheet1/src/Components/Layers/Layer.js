8362151c55490212558a3f10d49c7dd57a98d9c5
Randkill <randkill.randkill@gmail.com>
2020-08-18 14:23:22 +0430

    Layer Component created!

---
+++ MyOwnExercises/CheatSheet1/cheatsheet1/src/Components/Layers/Layer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Layer.css'
+
+const Layer = (props) => {
+    return(
+        <div className="Layer-Border">
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
+export default Layer;
