c89b1d9761c90bbe3998f36c2af761f94a75db21
Randkill <randkill.randkill@gmail.com>
2020-08-18 16:35:58 +0430

    Added some extra stylings!

--- MyOwnExercises/CheatSheet1/cheatsheet1/src/Components/Layers/Layer.js
+++ MyOwnExercises/CheatSheet1/cheatsheet1/src/Components/Layers/Layer.js
@@ -4,8 +4,8 @@ import './Layer.css'
 const Layer = (props) => {
     return(
         <div className="Layer-Border">
-            <h1>{props.info}</h1>
-            <input className="InputBox" style={{width:'80%'}} onChange={props.change}></input>
+            <h1 style={{margin: '20%'}}>{props.info}</h1>
+            <input className="InputBox" style={{width:'80%', margin: 'auto'}} onChange={props.change}></input>
             <button className="DeleteButton" onClick={props.delete}>-</button>
         </div>
     )
