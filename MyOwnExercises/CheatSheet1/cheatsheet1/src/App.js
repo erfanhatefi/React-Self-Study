ea80cfdd206e00e9328a2366dd42a6c61ec892d9
Randkill <randkill.randkill@gmail.com>
2020-08-18 15:34:34 +0430

    Now adding components via + button

--- MyOwnExercises/CheatSheet1/cheatsheet1/src/App.js
+++ MyOwnExercises/CheatSheet1/cheatsheet1/src/App.js
@@ -4,10 +4,41 @@ import './App.css';
 import Layer from './Components/Layers/Layer.js'
 
 class App extends Component {
+  state = {
+    layers: []
+  }
+
+  clickButton = () => {
+    console.log('Button Clicked !')
+
+    const tempLayers = [...this.state.layers]
+
+    tempLayers.push({info: '2'});
+    this.setState({layers: tempLayers})
+    //console.log('New Layers count:', tempLayers[0])
+    console.log(this.state)
+
+  }
+
   render() {
+    let Layers = null
+    
+    Layers = (
+      <div>
+        {
+          this.state.layers.map((layer) => {
+            return(
+              <Layer info={layer.info}/>
+            )
+          })
+        }
+      </div>
+    )
+    
     return (
       <div className="App">
-        <div><Layer/></div><div><Layer/></div>
+        {Layers}
+        <button className="AddButton" onClick={this.clickButton}>+</button>
       </div>
     );
   }
